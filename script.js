c9b4a4b19b6e1c56109d0e9181b7662319366882
KDDeveloper <81235505+KDDeveloper@users.noreply.github.com>
2021-08-20 18:34:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+setTimeout( function(){
+    let h1 = document.createElement('h1');
+    document.body.appendChild(h1);
+    h1.innerText = 10;
+    setTimeout( function(){
+        let h1 = document.createElement('h1');
+        document.body.appendChild(h1);
+        h1.innerText = 9;
+        setTimeout( function(){
+            let h1 = document.createElement('h1');
+            document.body.appendChild(h1);
+            h1.innerText = 8;
+            setTimeout( function(){
+                let h1 = document.createElement('h1');
+                document.body.appendChild(h1);
+                h1.innerText = 7;
+                setTimeout( function(){
+                    let h1 = document.createElement('h1');
+                    document.body.appendChild(h1);
+                    h1.innerText = 6;
+                    setTimeout( function(){
+                        let h1 = document.createElement('h1');
+                        document.body.appendChild(h1);
+                        h1.innerText = 5;
+                        setTimeout( function(){
+                            let h1 = document.createElement('h1');
+                            document.body.appendChild(h1);
+                            h1.innerText = 4;
+                            setTimeout( function(){
+                                let h1 = document.createElement('h1');
+                                document.body.appendChild(h1);
+                                h1.innerText = 3;
+                                setTimeout( function(){
+                                    let h1 = document.createElement('h1');
+                                    document.body.appendChild(h1);
+                                    h1.innerText = 2;
+                                    setTimeout( function(){
+                                        let h1 = document.createElement('h1');
+                                        document.body.appendChild(h1);
+                                        h1.innerText = 1;
+                                        setTimeout( function(){
+                                            let h1 = document.createElement('h1');
+                                            document.body.appendChild(h1);
+                                            h1.innerText = "Happy Independence Day!";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)    
+},1000)
